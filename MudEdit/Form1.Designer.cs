647061242211b1a3ff32c878851468eaca62cfde
deshi <mcdorgle@gmail.com>
2022-07-09 23:15:12 -0700

    Add project files.

---
+++ MudEdit/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace MudEdit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bankbooksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.classesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gangsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.messagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monstersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.racesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.roomsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.shopsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.spellsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textblocksToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.usersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monstersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shopsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.divideAllExpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiplyAllExpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restockAllShopsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editorsToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1227, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editorsToolStripMenuItem
+            // 
+            this.editorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionsToolStripMenuItem,
+            this.bankbooksToolStripMenuItem,
+            this.classesToolStripMenuItem,
+            this.gangsToolStripMenuItem,
+            this.itemsToolStripMenuItem1,
+            this.messagesToolStripMenuItem,
+            this.monstersToolStripMenuItem1,
+            this.racesToolStripMenuItem,
+            this.roomsToolStripMenuItem1,
+            this.shopsToolStripMenuItem1,
+            this.spellsToolStripMenuItem,
+            this.textblocksToolStripMenuItem1,
+            this.usersToolStripMenuItem1});
+            this.editorsToolStripMenuItem.Name = "editorsToolStripMenuItem";
+            this.editorsToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.editorsToolStripMenuItem.Text = "Editors";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // bankbooksToolStripMenuItem
+            // 
+            this.bankbooksToolStripMenuItem.Name = "bankbooksToolStripMenuItem";
+            this.bankbooksToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.bankbooksToolStripMenuItem.Text = "Bankbooks";
+            // 
+            // classesToolStripMenuItem
+            // 
+            this.classesToolStripMenuItem.Name = "classesToolStripMenuItem";
+            this.classesToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.classesToolStripMenuItem.Text = "Classes";
+            // 
+            // gangsToolStripMenuItem
+            // 
+            this.gangsToolStripMenuItem.Name = "gangsToolStripMenuItem";
+            this.gangsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.gangsToolStripMenuItem.Text = "Gangs";
+            // 
+            // itemsToolStripMenuItem1
+            // 
+            this.itemsToolStripMenuItem1.Name = "itemsToolStripMenuItem1";
+            this.itemsToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.itemsToolStripMenuItem1.Text = "Items";
+            // 
+            // messagesToolStripMenuItem
+            // 
+            this.messagesToolStripMenuItem.Name = "messagesToolStripMenuItem";
+            this.messagesToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.messagesToolStripMenuItem.Text = "Messages";
+            // 
+            // monstersToolStripMenuItem1
+            // 
+            this.monstersToolStripMenuItem1.Name = "monstersToolStripMenuItem1";
+            this.monstersToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.monstersToolStripMenuItem1.Text = "Monsters";
+            // 
+            // racesToolStripMenuItem
+            // 
+            this.racesToolStripMenuItem.Name = "racesToolStripMenuItem";
+            this.racesToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.racesToolStripMenuItem.Text = "Races";
+            // 
+            // roomsToolStripMenuItem1
+            // 
+            this.roomsToolStripMenuItem1.Name = "roomsToolStripMenuItem1";
+            this.roomsToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.roomsToolStripMenuItem1.Text = "Rooms";
+            // 
+            // shopsToolStripMenuItem1
+            // 
+            this.shopsToolStripMenuItem1.Name = "shopsToolStripMenuItem1";
+            this.shopsToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.shopsToolStripMenuItem1.Text = "Shops";
+            // 
+            // spellsToolStripMenuItem
+            // 
+            this.spellsToolStripMenuItem.Name = "spellsToolStripMenuItem";
+            this.spellsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.spellsToolStripMenuItem.Text = "Spells";
+            // 
+            // textblocksToolStripMenuItem1
+            // 
+            this.textblocksToolStripMenuItem1.Name = "textblocksToolStripMenuItem1";
+            this.textblocksToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.textblocksToolStripMenuItem1.Text = "Textblocks";
+            // 
+            // usersToolStripMenuItem1
+            // 
+            this.usersToolStripMenuItem1.Name = "usersToolStripMenuItem1";
+            this.usersToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.usersToolStripMenuItem1.Text = "Users";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.databaseToolStripMenuItem,
+            this.monstersToolStripMenuItem,
+            this.shopsToolStripMenuItem,
+            this.compileUpdatesToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem,
+            this.importToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // monstersToolStripMenuItem
+            // 
+            this.monstersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.divideAllExpToolStripMenuItem,
+            this.multiplyAllExpToolStripMenuItem});
+            this.monstersToolStripMenuItem.Name = "monstersToolStripMenuItem";
+            this.monstersToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.monstersToolStripMenuItem.Text = "Monsters";
+            // 
+            // shopsToolStripMenuItem
+            // 
+            this.shopsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restockAllShopsToolStripMenuItem});
+            this.shopsToolStripMenuItem.Name = "shopsToolStripMenuItem";
+            this.shopsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shopsToolStripMenuItem.Text = "Shops";
+            // 
+            // compileUpdatesToolStripMenuItem
+            // 
+            this.compileUpdatesToolStripMenuItem.Name = "compileUpdatesToolStripMenuItem";
+            this.compileUpdatesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.compileUpdatesToolStripMenuItem.Text = "Compile Updates?";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // divideAllExpToolStripMenuItem
+            // 
+            this.divideAllExpToolStripMenuItem.Name = "divideAllExpToolStripMenuItem";
+            this.divideAllExpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.divideAllExpToolStripMenuItem.Text = "Divide all exp";
+            // 
+            // multiplyAllExpToolStripMenuItem
+            // 
+            this.multiplyAllExpToolStripMenuItem.Name = "multiplyAllExpToolStripMenuItem";
+            this.multiplyAllExpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.multiplyAllExpToolStripMenuItem.Text = "Multiply all exp";
+            // 
+            // restockAllShopsToolStripMenuItem
+            // 
+            this.restockAllShopsToolStripMenuItem.Name = "restockAllShopsToolStripMenuItem";
+            this.restockAllShopsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restockAllShopsToolStripMenuItem.Text = "Restock all shops";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 656);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MudEdit Version 1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editorsToolStripMenuItem;
+        private ToolStripMenuItem actionsToolStripMenuItem;
+        private ToolStripMenuItem bankbooksToolStripMenuItem;
+        private ToolStripMenuItem classesToolStripMenuItem;
+        private ToolStripMenuItem gangsToolStripMenuItem;
+        private ToolStripMenuItem itemsToolStripMenuItem1;
+        private ToolStripMenuItem messagesToolStripMenuItem;
+        private ToolStripMenuItem monstersToolStripMenuItem1;
+        private ToolStripMenuItem racesToolStripMenuItem;
+        private ToolStripMenuItem roomsToolStripMenuItem1;
+        private ToolStripMenuItem shopsToolStripMenuItem1;
+        private ToolStripMenuItem spellsToolStripMenuItem;
+        private ToolStripMenuItem textblocksToolStripMenuItem1;
+        private ToolStripMenuItem usersToolStripMenuItem1;
+        private ToolStripMenuItem toolsToolStripMenuItem;
+        private ToolStripMenuItem databaseToolStripMenuItem;
+        private ToolStripMenuItem monstersToolStripMenuItem;
+        private ToolStripMenuItem shopsToolStripMenuItem;
+        private ToolStripMenuItem compileUpdatesToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem exportToolStripMenuItem;
+        private ToolStripMenuItem importToolStripMenuItem;
+        private ToolStripMenuItem divideAllExpToolStripMenuItem;
+        private ToolStripMenuItem multiplyAllExpToolStripMenuItem;
+        private ToolStripMenuItem restockAllShopsToolStripMenuItem;
+    }
+}
